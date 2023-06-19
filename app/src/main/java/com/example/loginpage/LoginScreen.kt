ea9548e976892f7409a4afe5fbe5e99d68ce11eb
HiPiyushh <hipiyushh@gmail.com>
2023-06-19 23:59:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loginpage/LoginScreen.kt
@@ -0,0 +1,40 @@
+package com.example.loginpage
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun LoginScreen(name: String, modifier: Modifier = Modifier) {
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(id = R.drawable.apna), contentDescription = "loginImage")
+
+        Spacer(modifier = Modifier.height(200.dp))
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text =  "$name",
+                fontSize = 26.sp)
+            Spacer(modifier = Modifier.height(24.dp))
+            Button(onClick = { /*TODO*/ },
+                shape = CircleShape
+            ) {
+                Text(text = "Login")
+
+            }
+
+        }
+    }
+
+}
